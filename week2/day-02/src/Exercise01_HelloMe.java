f8bdf0befe3124e03885ee93d039b1d998a7b03d
Aradi Nóra <aradinora111@gmail.com>
2017-03-21 16:33:13 +0100

    add 'printing to the terminal' type exercises 01-03

---
+++ week2/day-02/src/Exercise01_HelloMe.java
@@ -0,0 +1,7 @@
+public class Exercise01_HelloMe {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Nora");
+    }
+}
---
+++ week2/day-02/src/Exercise02_HelloOthers.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Nóra on 2017. 03. 21..
+ */
+public class Exercise02_HelloOthers {
+    public static void main(String[] args){
+        System.out.println("Hello, Noemi!");
+        System.out.println("Hello, Dani!");
+        System.out.println("Hello, Kristof!");
+    }
+}
