3043f8e6e4151dc1d4f0e03ed39c4265ae0db19e
Aradi Nóra <aradinora111@gmail.com>
2017-03-22 17:34:49 +0100

    add week2/day-03 array related exercises - exercise 18

---
+++ week2/day-03/src/Ex18_SumAllElements.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Nóra on 2017. 03. 22..
+ */
+public class Ex18_SumAllElements {
+  public static void main(String[] args) {
+    int[] ai = {3,4,5,6,7};
+    int sum = 0;
+    for (int i=0; i<ai.length; i++) {
+      sum += ai[i];
+    }
+    System.out.println(sum);
+  }
+}
