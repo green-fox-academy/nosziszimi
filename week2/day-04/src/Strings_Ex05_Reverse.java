a6a35528a53f3945a09475de5c0f60ec26e8836b
Aradi Nóra <aradinora111@gmail.com>
2017-03-23 13:59:05 +0100

    add week2/day-04/Strings_Ex05_Reverse.java done

--- week2/day-04/src/Strings_Ex05_Reverse.java
+++ week2/day-04/src/Strings_Ex05_Reverse.java
@@ -4,12 +4,12 @@
 public class Strings_Ex05_Reverse {
   public static void main(String[] args) {
     String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
-
+    String temp = "";
     int length = reversed.length();
     for (int i=length-1; i>=0; i--){
-      reversed = reversed +(reversed.charAt(i));
+      temp = temp + (reversed.charAt(i));
 
     }
-    System.out.println(reversed);
+    System.out.println(temp);
   }
 }
