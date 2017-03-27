aa2253e44bb9f02bbddcf050b3198aeacc628d28
Aradi Nóra <aradinora111@gmail.com>
2017-03-27 11:46:10 +0200

    Lotto exercise - put all numbers in an ArrayList

--- week-03/day-01/src/Ex_01_Lotto.java
+++ week-03/day-01/src/Ex_01_Lotto.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,13 +10,19 @@ import java.util.List;
  */
 public class Ex_01_Lotto {
   public static void main(String[] args) {
+    ArrayList<String> numbers = new ArrayList<>();
     try {
       Path path = Paths.get("assets/otos.csv");
       List<String> lines = Files.readAllLines(path);
       for (String line : lines){
-        String[] array = line.split(";");
-        System.out.println(array[11]);
+        String[] csvColums = line.split(";");
+        numbers.add(csvColums[11]);
+        numbers.add(csvColums[12]);
+        numbers.add(csvColums[13]);
+        numbers.add(csvColums[14]);
+        numbers.add(csvColums[15]);
       }
+      numbers.toString()
     }
     catch (Exception e) {
       e.printStackTrace();
