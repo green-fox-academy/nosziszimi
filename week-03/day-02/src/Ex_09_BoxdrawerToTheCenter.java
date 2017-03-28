b96070c09a5238efa3ab68d2c47c898f96a10568
Aradi Nóra <aradinora111@gmail.com>
2017-03-28 12:15:06 +0200

    add exercise09: squaredrawer function - takes the size parameter, draws square in the center

---
+++ week-03/day-02/src/Ex_09_BoxdrawerToTheCenter.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+/**
+ * Created by Nóra on 2017. 03. 28..
+ */
+public class Ex_09_BoxdrawerToTheCenter {
+  public static void mainDraw(Graphics graphics){
+    // create a square drawing function that takes 1 parameter:
+    // the square size
+    // and draws a square of that size to the center of the canvas.
+    // draw 3 squares with that function.
+    squareDrawer(50, graphics);
+    squareDrawer(60, graphics);
+    squareDrawer(100,graphics);
+}
+  public static void squareDrawer (int size, Graphics graphics){
+    graphics.setColor(Color.BLUE);
+    graphics.drawRect(150-(size/2), 150-(size/2), size, size);
+}
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(320, 343));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
