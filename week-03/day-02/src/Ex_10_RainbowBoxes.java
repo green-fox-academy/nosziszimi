0211c6a32cfe415243799a8aedd85fed6200185e
Aradi Nóra <aradinora111@gmail.com>
2017-03-28 12:45:17 +0200

    add exercise10: squaredrawer function - takes the size and color parameter, draws square in the center

---
+++ week-03/day-02/src/Ex_10_RainbowBoxes.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+/**
+ * Created by Nóra on 2017. 03. 28..
+ */
+public class Ex_10_RainbowBoxes {
+  public static void mainDraw(Graphics graphics){
+    // create a square drawing function that takes 2 parameters:
+    // the square size, and the fill color,
+    // and draws a square of that size and color to the center of the canvas.
+    // create a loop that fills the canvas with rainbow colored squares.
+    squareDrawer(50, Color.RED, graphics);
+}
+  public static void squareDrawer (int size, java.awt.Color color,Graphics graphics){
+    graphics.setColor(color);
+    graphics.fillRect(150-(size/2), 150-(size/2), size, size);
+  }
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(320, 343));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
