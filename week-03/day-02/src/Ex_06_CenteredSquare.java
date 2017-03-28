4376c6e6698e691285c696b98b465f0e452546a9
Aradi Nóra <aradinora111@gmail.com>
2017-03-28 11:46:13 +0200

    add exercise06: Drawing a square in the center

---
+++ week-03/day-02/src/Ex_06_CenteredSquare.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+/**
+ * Created by Nóra on 2017. 03. 28..
+ */
+public class Ex_06_CenteredSquare {
+  public static void mainDraw(Graphics graphics){
+    // draw a green 10x10 square to the canvas' center.
+   graphics.setColor(Color.GREEN);
+    graphics.drawRect(145, 145, 10,10);
+  }
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(320, 343));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
