f6075b69e60611e22fa8fed93bd50f3774f0191b
Aradi Nóra <aradinora111@gmail.com>
2017-04-03 12:21:13 +0200

    add Domino exercise

---
+++ week-04/day-01/UseClassExercises/src/Domino.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+/**
+ * Created by Nóra on 2017. 04. 03..
+ */
+public class Domino {
+  private final int[] values;
+
+  public Domino(int valueA, int valueB) {
+    this.values = new int[]{valueA, valueB};
+  }
+
+  public int[] getValues() {
+    return values;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + values[0] + ", " + values[1] + "]";
+  }
+}
---
+++ week-04/day-01/UseClassExercises/src/Ex_03_Dominoes.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by Nóra on 2017. 04. 03..
+ */
+public class Ex_03_Dominoes {
+  public static void main(String[] args) {
+    List<Domino> dominoes = initializeDominoes();
+    // You have the list of Dominoes
+    // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+    // eg: [2, 4], [4, 3], [3, 5] ...
+
+    System.out.println(dominoes);
+
+    dominoSneak();
+  }
+
+  static List<Domino> initializeDominoes() {
+    List<Domino> dominoes = new ArrayList<>();
+    dominoes.add(new Domino(5, 2));
+    dominoes.add(new Domino(4, 6));
+    dominoes.add(new Domino(1, 5));
+    dominoes.add(new Domino(6, 7));
+    dominoes.add(new Domino(2, 4));
+    dominoes.add(new Domino(7, 1));
+    return dominoes;
+  }
+  public static void dominoSneak(){
+    List<Domino> orderedDominoes = new ArrayList<>();
+    List<Domino> dominoes = initializeDominoes();
+    orderedDominoes.add(dominoes.get(0));
+    for (int j = 0; dominoes.size() != orderedDominoes.size(); j++){
+      for (int i=0; i < dominoes.size() ; i++) {
+        int[] actualDominoValues = (dominoes.get(i)).getValues();
+        if (actualDominoValues[0] == ((orderedDominoes.get(orderedDominoes.size()-1).getValues())[1])){
+          orderedDominoes.add(dominoes.get(i));
+        }
+    }
+
+    }
+    System.out.println(orderedDominoes);
+  }
+
+}
+
+
