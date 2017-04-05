1be2e59df5e44b5b4055f313961ddb4482e87dc3
Aradi Nóra <aradinora111@gmail.com>
2017-04-05 15:49:13 +0200

    add Coows And Bulls exercise - create guess method

--- week-04/day-03/src/cowsandbulls/CowsAndBulls.java
+++ week-04/day-03/src/cowsandbulls/CowsAndBulls.java
@@ -5,8 +5,19 @@ import java.util.ArrayList;
 /**
  * Created by Nóra on 2017. 04. 05..
  */
+
+// Create a class what is capable of playing exactly one game of Cows and Bulls (CAB).
+// The player have to guess a 4 digit number. For every digit that the player
+// guessed correctly in the correct place, they have a “cow”.
+// For every digit the player guessed correctly in the wrong place is a “bull.”
+//The CAB object should have a random 4 digit number, which is the goal to guess.
+//The CAB object should have a state where the game state is stored (playing, finished).
+//The CAB object should have a counter where it counts the guesses.
+//The CAB object should have a guess method, which returns a string of the guess result.
+//All methods, including constructor should be tested .
+
 public class CowsAndBulls {
-  int numberToGuess, guessCounter;
+  int numberToGuess, guessCounter, cowCounter, bullCounter;
   boolean gameFinished;
 
   public CowsAndBulls() {
@@ -25,7 +36,24 @@ public class CowsAndBulls {
   }
 
   String guess(int guessedNumber) {
-
+    ArrayList<Integer> digitsOfNumberToGuess = separateDigits(numberToGuess);
+    ArrayList<Integer> digitsOfguess = separateDigits(guessedNumber);
+    cowCounter = 0;
+    bullCounter = 0;
+    for (int i = 3; i < 0 ; i--) {
+      if (digitsOfNumberToGuess.get(i) == digitsOfguess.get(i)) {
+        cowCounter++;
+      } else {
+        for (int j = 3; j < 0 ; j++) {
+          if (i != j) {
+            if (digitsOfNumberToGuess.get(i) == digitsOfguess.get(j)){
+              bullCounter++;
+            }
+          }
+        }
+      }
+    }
+    return "You have " + cowCounter + " cow, and " + bullCounter + " bull.";
   }
 
 
