d70879f3f866cc5aa7b2ac2439bb5f2159d46bce
Aradi Nóra <aradinora111@gmail.com>
2017-04-10 12:27:08 +0200

    RPG - create classes

---
+++ week-05/src/Area.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Nóra on 2017. 04. 10..
+ */
+public class Area extends GameObject {
+
+}
---
+++ week-05/src/Tile.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Nóra on 2017. 04. 10..
+ */
+public class Tile extends Area{
+
+}
