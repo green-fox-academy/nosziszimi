33e0946bd6a1907d0e7d1464871f3ccb6ada0cca
Aradi Nóra <aradinora111@gmail.com>
2017-04-13 10:49:27 +0200

    RPG - add HP, SP, DP fields to the Hero class

--- week-05/src/Hero.java
+++ week-05/src/Hero.java
@@ -4,9 +4,14 @@ import java.awt.image.BufferedImage;
  * Created by Nóra on 2017. 04. 10..
  */
 public class Hero extends Character {
+  int maxHP, currentHP, dP, sP;
 
   public Hero() {
     super(0, 0);
     image = ImageLoader.getInstance().HERO_DOWN;
+    maxHP = 10;
+    currentHP = 10;
+    dP = 8;
+    sP = 6;
   }
 }
