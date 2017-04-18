ba43b8005e7c723a710216024746ed1ce4082abb
Aradi Nóra <aradinora111@gmail.com>
2017-04-18 15:59:19 +0200

    Instrument - add StringedInstrument class

---
+++ week-06/day-02/src/main/java/music/StringedInstrument.java
@@ -0,0 +1,10 @@
+package main.java.music;
+
+/**
+ * Created by Nóra on 2017. 04. 18..
+ */
+public abstract class StringedInstrument extends Instrument{
+  int numberOfStrings;
+  final String formatForPlay = "%s, a %d-stringed instrument that %s\n";
+  
+}
