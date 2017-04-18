7b6c4eba31f0634f91fb72854ba863fd9570a3c0
Aradi Nóra <aradinora111@gmail.com>
2017-04-18 16:25:38 +0200

    Instrument - reformat BassGuitar class

--- week-06/day-02/src/main/java/music/BassGuitar.java
+++ week-06/day-02/src/main/java/music/BassGuitar.java
@@ -4,6 +4,7 @@ package main.java.music;
  * Created by Nóra on 2017. 04. 18..
  */
 public class BassGuitar extends StringedInstrument {
+
   public BassGuitar() {
     numberOfStrings = 4;
     sound = "Duum-duum-duum";
