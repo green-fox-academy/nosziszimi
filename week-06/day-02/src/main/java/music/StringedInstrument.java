691c2788815ca29b967454c6089a23351161660c
Aradi Nóra <aradinora111@gmail.com>
2017-04-18 16:04:35 +0200

    Instrument - add play() method to StringedInstrument class

--- week-06/day-02/src/main/java/music/StringedInstrument.java
+++ week-06/day-02/src/main/java/music/StringedInstrument.java
@@ -5,6 +5,11 @@ package main.java.music;
  */
 public abstract class StringedInstrument extends Instrument{
   int numberOfStrings;
+  String sound;
   final String formatForPlay = "%s, a %d-stringed instrument that %s\n";
-  
+
+  @Override
+  public void play() {
+    System.out.printf(formatForPlay, name, numberOfStrings, sound);
+  }
 }
