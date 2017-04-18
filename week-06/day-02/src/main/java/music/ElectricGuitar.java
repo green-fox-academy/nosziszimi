ff48e97250a54cd995299a37308cbaf17840f0b1
Aradi Nóra <aradinora111@gmail.com>
2017-04-18 16:12:45 +0200

    Instrument - create ElectricGuitar class

---
+++ week-06/day-02/src/main/java/music/ElectricGuitar.java
@@ -0,0 +1,12 @@
+package main.java.music;
+
+/**
+ * Created by Nóra on 2017. 04. 18..
+ */
+public class ElectricGuitar extends StringedInstrument {
+
+  public ElectricGuitar() {
+    numberOfStrings = 6;
+    sound = "Twang";
+  }
+}
