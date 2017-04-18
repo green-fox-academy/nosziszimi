8298a15624957c0f5096276975b4aef2cdfef4c7
Aradi Nóra <aradinora111@gmail.com>
2017-04-18 16:31:35 +0200

    Instrument - add an other constructor to the ElectricGuitar class

--- week-06/day-02/src/main/java/music/ElectricGuitar.java
+++ week-06/day-02/src/main/java/music/ElectricGuitar.java
@@ -6,7 +6,11 @@ package main.java.music;
 public class ElectricGuitar extends StringedInstrument {
 
   public ElectricGuitar() {
-    numberOfStrings = 6;
+    this(6);
+  }
+
+  public ElectricGuitar(int strings) {
+    numberOfStrings = strings;
     sound = "Twang";
   }
 }
