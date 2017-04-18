60a2a1bdfe9c55902d9e6be3b4e76dbd21f46913
Aradi Nóra <aradinora111@gmail.com>
2017-04-18 16:15:02 +0200

    Instrument - create BassGuitar class

---
+++ week-06/day-02/src/main/java/music/BassGuitar.java
@@ -0,0 +1,11 @@
+package main.java.music;
+
+/**
+ * Created by Nóra on 2017. 04. 18..
+ */
+public class BassGuitar extends StringedInstrument {
+  public BassGuitar() {
+    numberOfStrings = 4;
+    sound = "Duum-duum-duum";
+  }
+}
