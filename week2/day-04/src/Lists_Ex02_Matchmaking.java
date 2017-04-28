db0068c56b450725a5b667ab392f240004dc49a2
Aradi Nóra <aradinora111@gmail.com>
2017-04-28 20:21:14 +0200

    reformat esercise 2 of week-02 day-04

--- week2/day-04/src/Lists_Ex02_Matchmaking.java
+++ week2/day-04/src/Lists_Ex02_Matchmaking.java
@@ -4,29 +4,27 @@ import java.util.*;
  * Created by Nóra on 2017. 03. 23..
  */
 public class Lists_Ex02_Matchmaking {
+
   public static void main(String[] args) {
-    ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Bözsi","Kat","Jane"));
-    ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Béla","Todd","Neef","Jeff"));
+    ArrayList<String> girls = new ArrayList<String>(
+            Arrays.asList("Eve", "Ashley", "Bözsi", "Kat", "Jane"));
+    ArrayList<String> boys = new ArrayList<String>(
+            Arrays.asList("Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"));
     ArrayList<String> order = new ArrayList<String>();
 
     int boysSize = boys.size();
     int girlsSize = girls.size();
     int size = 0;
 
-    if (boysSize >= girlsSize){
+    if (boysSize >= girlsSize) {
       size = boysSize;
-    }
-    else {
+    } else {
       size = girlsSize;
     }
-
-
-    for (int i=0; i<size; i++){
-
-      if (i < girlsSize){
+    for (int i = 0; i < size; i++) {
+      if (i < girlsSize) {
         order.add(girls.get(i));
       }
-
       if (i < boysSize) {
         order.add(boys.get(i));
       }
