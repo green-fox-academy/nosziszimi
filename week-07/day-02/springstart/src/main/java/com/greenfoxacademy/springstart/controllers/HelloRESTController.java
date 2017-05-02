4abf01770b8371abe0fb694ab99d05199c0a2b63
Aradi Nóra <aradinora111@gmail.com>
2017-05-02 12:51:21 +0200

    add Hello User REST App

--- week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ public class HelloRESTController {
 
   @RequestMapping(value="/greeting")
   @ResponseBody
-  public Greeting greeting() {
-    return Greeting.of(1, "Hello World!");
+  public Greeting greeting(@RequestParam("name")String name) {
+    return Greeting.of(1, "Hello " + name + "!");
   }
 }
