f581025e5cb051cedb5de2ff9cb0f26b176b4a93
Aradi Nóra <aradinora111@gmail.com>
2017-05-03 16:51:43 +0200

    Thymeleaf exercises - add Exercise9: show bad&good guy using switch and refactor template using table

---
+++ week-07/day-03/mvc/src/main/java/com/greenfox/nori/mvc/controllers/Ex9Controller.java
@@ -0,0 +1,34 @@
+package com.greenfox.nori.mvc.controllers;
+
+import com.greenfox.nori.mvc.model.BankAccount;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Nóra on 2017. 05. 03..
+ */
+@Controller
+public class Ex9Controller {
+
+  @RequestMapping("/Exercise9")
+  public String showElements(Model model) {
+    List<BankAccount> bankAccounts = new ArrayList<>();
+    bankAccounts.add(new BankAccount
+            ("Rafiki", 800, "mandrill", false, false));
+    bankAccounts.add(new BankAccount
+            ("Simba", 1000, "lion", true, false));
+    bankAccounts.add(new BankAccount
+            ("Nala", 1000, "lion", false, false));
+    bankAccounts.add(new BankAccount
+            ("Zordon", 500, "lion", false, true));
+    bankAccounts.add(new BankAccount
+            ("Timon", 600, "meerkat", false, false));
+    bankAccounts.add(new BankAccount
+            ("Pumba", 700, "warthog", false, false));
+    model.addAttribute("list", bankAccounts);
+    return "ex9";
+  }
+}
