51fa65dbf5012351b35f23f27101559d53afeaa2
Aradi Nóra <aradinora111@gmail.com>
2017-05-09 17:35:33 +0200

    Todo  MySQL- list todos in the browser with all fields and borders

--- week-08/day-02/todomysql/src/main/java/com/greenfox/TodoApplication.java
+++ week-08/day-02/todomysql/src/main/java/com/greenfox/TodoApplication.java
@@ -10,8 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoApplication implements CommandLineRunner {
 
-  @Autowired
-  private Repository repo;
+  //@Autowired
+  //private Repository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
@@ -19,9 +19,10 @@ public class TodoApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    repo.save(new Todo("valami"));
-    repo.save(new Todo("valami2"));
-    repo.save(new Todo("valami3"));
+    //repo.save(new Todo(0, "valami", true, true));
+    //repo.save(new Todo(0, "valami2", true, false));
+    //repo.save(new Todo(0, "valami3", false, true));
+
   }
 }
 
--- week-08/day-02/todomysql/src/main/java/com/greenfox/model/Todo.java
+++ week-08/day-02/todomysql/src/main/java/com/greenfox/model/Todo.java
@@ -5,13 +5,19 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by Nóra on 2017. 05. 09..
  */
+
 @Entity
+@AllArgsConstructor
+@Getter
+@Setter
 public class Todo {
   @Id
   @GeneratedValue(strategy= GenerationType.AUTO)
@@ -31,5 +37,13 @@ public class Todo {
 
   public Todo(String title) {
     this.title = title;
+  }
+
+  public boolean getIsUrgent() {
+    return isUrgent;
+  }
+
+  public boolean getIsDone() {
+    return isDone;
   }
 }
