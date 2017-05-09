41ed12e4b99e07e44a48bb536fc801aaed4f4f08
Aradi Nóra <aradinora111@gmail.com>
2017-05-09 11:24:02 +0200

    Todo - add working web application

--- week-08/day-02/todo/src/main/java/com/greenfox/controller/TodoController.java
+++ week-08/day-02/todo/src/main/java/com/greenfox/controller/TodoController.java
@@ -1,8 +1,19 @@
 package com.greenfox.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by Nóra on 2017. 05. 09..
  */
+@Controller
+@RequestMapping("/todo")
 public class TodoController {
-
+  @RequestMapping(value={"/", "/list"})
+  @ResponseBody
+  public String list(Model model) {
+    return "This is my first todo";
+  }
 }
--- week-08/day-02/todo/src/main/java/com/greenfox/model/Todo.java
+++ week-08/day-02/todo/src/main/java/com/greenfox/model/Todo.java
@@ -4,5 +4,8 @@ package com.greenfox.model;
  * Created by Nóra on 2017. 05. 09..
  */
 public class Todo {
-
+  int id;
+  String title;
+  boolean isUrgent = false;
+  boolean isDone = false;
 }
