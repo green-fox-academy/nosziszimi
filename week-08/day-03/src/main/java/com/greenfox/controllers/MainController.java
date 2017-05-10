e448b2bf09429458235d0d316ecdadc0d3842b20
Aradi Nóra <aradinora111@gmail.com>
2017-05-10 10:52:57 +0200

    REST backend - add the project with the frontend created

--- week-08/day-03/src/main/java/com/greenfox/controllers/MainController.java
+++ week-08/day-03/src/main/java/com/greenfox/controllers/MainController.java
@@ -1,8 +1,17 @@
 package com.greenfox.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Nóra on 2017. 05. 10..
  */
+@Controller
 public class MainController {
 
+  @RequestMapping("/")
+  public String index(Model model) {
+    return "index";
+  }
 }
