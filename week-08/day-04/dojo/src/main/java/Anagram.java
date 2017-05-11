790f6792903dd549410b393a0c9a9b2348f9feee
Aradi Nóra <aradinora111@gmail.com>
2017-05-11 11:07:50 +0200

    Anagram - add exercise

---
+++ week-08/day-04/dojo/src/main/java/Anagram.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Nóra on 2017. 05. 11..
+ */
+public class Anagram {
+
+  public static boolean isAnagram(String s1, String s2) {
+    if (s1 == null || s2 == null) {
+      return false;
+    } else {
+      s1 = s1.toLowerCase();
+      s2 = s2.toLowerCase();
+      if (sort(s1).equals(sort(s2))) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  public static String sort(String incomingString) {
+    for (int i = 0; i < incomingString.length(); i++) {
+      for (int j = i; j < incomingString.length(); j++) {
+        if (incomingString.charAt(i) > incomingString.charAt(j)) {
+          incomingString = incomingString.substring(0, i)
+                  + incomingString.charAt(j)
+                  + incomingString.substring(i + 1, j)
+                  + incomingString.charAt(i)
+                  + incomingString.substring(j + 1, incomingString.length());
+        }
+      }
+    }
+    return incomingString.trim();
+  }
+}
---
+++ week-08/day-04/dojo/src/test/java/AnagramTest.java
@@ -0,0 +1,48 @@
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+/**
+ * Created by Nóra on 2017. 05. 11..
+ */
+public class AnagramTest {
+
+  @Test
+  public void TestEqualStrings() {
+    assertEquals(true, Anagram.isAnagram("dog", "dog"));
+  }
+
+  @Test
+  public void TestEqualStringsAnagram() {
+    assertEquals(true, Anagram.isAnagram("dog", "god"));
+  }
+
+  @Test
+  public void TestEqualStringsNotAnagram() {
+    assertEquals(false, Anagram.isAnagram("dog", "foo"));
+  }
+
+  @Test
+  public void TestAnagramWithCapitalLetters() {
+    assertEquals(true, Anagram.isAnagram("dog", "Dog"));
+  }
+
+  @Test
+  public void TestAnagramWithSpaces() {
+    assertEquals(true, Anagram.isAnagram("dog", "d og"));
+  }
+
+  @Test
+  public void TestAnagramWithOneNullString() {
+    String s = null;
+    assertEquals(false, Anagram.isAnagram("dog", s));
+  }
+
+  @Test
+  public void TestAnagramWithTwoNullStrings() {
+    String s = null;
+    assertEquals(false, Anagram.isAnagram(null , s));
+  }
+
+}
