fb4585507391537dc10c08847746e0387ef90ef7
Aradi Nóra <aradinora111@gmail.com>
2017-05-12 12:15:46 +0200

    Reddit - fix typo in class name

--- week-08/day-05/src/main/java/com/greenfox/nori/reddit/controller/PostController.java
+++ week-08/day-05/src/main/java/com/greenfox/nori/reddit/controller/PostController.java
@@ -5,6 +5,8 @@ import com.greenfox.nori.reddit.model.PostList;
 import com.greenfox.nori.reddit.services.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,5 +26,10 @@ public class PostControler {
     repo.save(new Post());
     postList.setPosts((repo.findAll()));
     return postList;
+  }
+
+  @PostMapping("/posts")
+  public Post addpost(@RequestBody Post post) {
+    return post;
   }
 }
