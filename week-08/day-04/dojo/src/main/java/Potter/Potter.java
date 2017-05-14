d72f3f4377183cb580f5300ef5c5790766b5ceb8
Aradi Nóra <aradinora111@gmail.com>
2017-05-14 18:51:40 +0200

    Potter - add exercise

--- week-08/day-04/dojo/src/main/java/Potter/Potter.java
+++ week-08/day-04/dojo/src/main/java/Potter/Potter.java
@@ -1,8 +1,81 @@
 package Potter;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Nóra on 2017. 05. 11..
  */
 public class Potter {
 
+  public static float priceCounter(List<Integer> books) {
+    List<Integer> booksCopy = new ArrayList<>();
+    Collections.copy(booksCopy, books);
+    Collections.sort(booksCopy);
+    Map<Integer, Integer> bookOccurrences = getBookOccurrences(booksCopy);
+
+    List<List<Integer>> differentBookSets = getDifferentBookSets(booksCopy);
+
+    switch (books.size()) {
+      case 1:
+        return 8f;
+      case 2:
+        if (books.get(0) != books.get(1)) {
+          float cost = (2 * 8) * 0.95f;
+          return cost;
+        } else {
+          float cost = (float) (2 * 8);
+          return cost;
+        }
+      case 3:
+
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      default:
+        break;
+    }
+
+    if (books.size() == 2) {
+      if (books.get(0) != books.get(1)) {
+        float cost = (2 * 8) * 0.95f;
+        return cost;
+      } else {
+        float cost = (float) (2 * 8);
+        return cost;
+      }
+    } else {
+      return 8f;
+    }
+  }
+
+  private static List<List<Integer>> getDifferentBookSets(List<Integer> books) {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> oneSetOfDifferentBooks = new ArrayList<>();
+    List<Integer> remaining = new ArrayList<>();
+
+
+
+
+    return result;
+  }
+
+  private static Map<Integer, Integer> getBookOccurrences(List<Integer> books) {
+    Map<Integer, Integer> result = new HashMap<>();
+
+    for (Integer bookItem : books) {
+      if (result.containsKey(bookItem)) {
+        int previousAmount = result.get(bookItem);
+        result.put(bookItem, ++previousAmount);
+      } else {
+        result.put(bookItem, 1);
+      }
+    }
+    return result;
+  }
 }
--- week-08/day-04/dojo/src/test/java/Potter/PotterTest.java
+++ week-08/day-04/dojo/src/test/java/Potter/PotterTest.java
@@ -1,8 +1,34 @@
+package Potter;
+
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import org.junit.Test;
 
 /**
  * Created by Nóra on 2017. 05. 11..
  */
 public class PotterTest {
+
+  @Test
+  public void priceCounter_OneBook() throws Exception {
+  assertEquals(8f, Potter.priceCounter(Arrays.asList(1)), 0.001f);
+  }
+
+  @Test
+  public void priceCounter_Different2() throws Exception {
+  assertEquals(15.2f, Potter.priceCounter(Arrays.asList(1, 2)), 0.001f);
+  }
+
+  @Test
+  public void priceCounter_Same2() throws Exception {
+    assertEquals(16f, Potter.priceCounter(Arrays.asList(1, 1)), 0.001f);
+  }
+
+  @Test
+  public void priceCounter_Different3() throws Exception {
+    assertEquals(21.6f, Potter.priceCounter(Arrays.asList(1, 2, 3)), 0.001f);
+  }
+
 
 }
