d29d00f41df0b46370db9f25e3ebe252370d3605
Aradi Nóra <aradinora111@gmail.com>
2017-07-10 10:01:26 +0200

    add some more tests

--- week-16/src/ExerciseTest.java
+++ week-16/src/ExerciseTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.Test;
@@ -18,5 +19,39 @@ public class ExerciseTest {
     List<List<Integer>> matrix = Arrays.asList(list1, list2, list3);
 
     assertEquals(30,Exercise.sumOfMatrix(matrix));
+  }
+
+
+  @Test
+  public void sumOfMatrixIncludingNegativeNumbers() {
+    List<Integer> list1 = Arrays.asList(1, 2, -3, 4);
+    List<Integer> list2 = Arrays.asList(1, -2, 3, 4);
+    List<Integer> list3 = Arrays.asList(1, 2, 3, 4);
+
+    List<List<Integer>> matrix = Arrays.asList(list1, list2, list3);
+
+    assertEquals(20,Exercise.sumOfMatrix(matrix));
+  }
+
+  @Test
+  public void sumOfMatrixIncludingNullList() {
+    List<Integer> list1 = Arrays.asList(1, 2, -3, 4);
+    List<Integer> list2 = new ArrayList<>();
+    List<Integer> list3 = Arrays.asList(1, 2, 3, 4);
+
+    List<List<Integer>> matrix = Arrays.asList(list1, list2, list3);
+
+    assertEquals(14,Exercise.sumOfMatrix(matrix));
+  }
+
+  @Test
+  public void sumOfMatrixWithDifferentLengths() {
+    List<Integer> list1 = Arrays.asList(1, 2, -3, 4);
+    List<Integer> list2 = Arrays.asList(1);
+    List<Integer> list3 = Arrays.asList(1, 2, 4);
+
+    List<List<Integer>> matrix = Arrays.asList(list1, list2, list3);
+
+    assertEquals(12,Exercise.sumOfMatrix(matrix));
   }
 }
