f57f26f609b412a9d05e2c6338b3b6b0525aa872
Aradi Nóra <aradinora111@gmail.com>
2017-07-10 09:47:00 +0200

    use matrix summing function

--- week-16/src/Exercise.java
+++ week-16/src/Exercise.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -7,7 +8,13 @@ import java.util.List;
 public class Exercise {
 
   public static void main(String[] args) {
+    List<Integer> list1 = Arrays.asList(1, 2, 3, 4);
+    List<Integer> list2 = Arrays.asList(1, 2, 3, 4);
+    List<Integer> list3 = Arrays.asList(1, 2, 3, 4);
 
+    List<List<Integer>> matrix = Arrays.asList(list1, list2, list3);
+
+    System.out.println(sumOfMatrix(matrix));
   }
 
   public static int sumOfMatrix(List<List<Integer>> matrix) {
